f987f9377ce00240539569a6cfa62f57c68280f9
Maksim Saenko <mm.saenko@gmail.com>
2021-04-03 17:22:25 +0300

    fix vim-rooter config

--- lua/settings/plugins/vim-rooter.lua
+++ lua/settings/plugins/vim-rooter.lua
@@ -1,5 +1,5 @@
 vim.g.rooter_silent_chdir = 1
-vim.g.non_project_files = 'current'
+vim.g.rooter_change_directory_for_non_project_files = 'current'
 vim.g.rooter_patterns = {
   '.git',
   '.gitignore',
